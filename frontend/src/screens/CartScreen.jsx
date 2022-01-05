6986e272d3a1eb424f479bf831dc888259ec045b
avinashsoni9829 <avinashsoni9829@gmail.com>
2022-01-05 17:41:03 +0530

    issuesresolved

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { useDispatch , useSelector } from 'react-redux';
+import {Row , Col , Form , Button , Card , Image , ListGroup} from 'react-bootstrap';
+import { addToCart } from '../actions/cartAction';
 
-const CartScreen = () => {
+const CartScreen = ({match,location,history}) => {
+    const productId = match.params.id
+    const qty = location.search ? location.search.split('=')[1] : 1;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if(productId){
+             dispatch(addToCart(productId, qty))
+        }
+    },[dispatch , productId, qty]);
+
+    const cart = useSelector((state => state.cart));
+
+    const {cartItems} = cart;
+
+    console.log(cart);
+    
+
+
     return (
         <div>
             <h1>Cart Screen</h1>
